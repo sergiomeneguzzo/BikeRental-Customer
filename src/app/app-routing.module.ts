d5f1d195d83ff5f824f864049cda1273c7df8387
sergiomeneguzzo <sergio03.mene@gmail.com>
2025-06-09 11:54:52 +0200

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
-import { homeGuard } from './guards/home.guard';
-import { authGuard } from './guards/auth.guard';
+import {BookingComponent} from './pages/booking/booking.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent, canActivate: [homeGuard] },
-  { path: 'register', component: RegisterComponent, canActivate: [homeGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent},
+  { path: 'booking', component: BookingComponent},
+
 ];
 
 @NgModule({
