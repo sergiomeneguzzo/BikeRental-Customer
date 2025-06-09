df22a074eed2b71f3bd14648e7ba9e716c595669
Stefano Agbodan <mystefano95@gmail.com>
2025-06-09 21:44:53 +0200

    Updated home navbar

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import Lenis from 'lenis'
+import { gsap } from 'gsap';
 
 platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true,
-})
-  .catch(err => console.error(err));
+}).catch(err => console.error(err));
+
+
+const lenis = new Lenis({ duration: 1.2, easing: t => Math.min(1, 1.001 - Math.pow(2, -10 * t)) });
+function raf(time: number) { lenis.raf(time); requestAnimationFrame(raf); }
+requestAnimationFrame(raf);
+
