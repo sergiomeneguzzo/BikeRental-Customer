ac5376202fff873ee2d1849e5c4ff5979148f00d
sergiomeneguzzo <sergio03.mene@gmail.com>
2025-06-11 20:05:11 +0200

    Menu Mobile Navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
-import {Component, ElementRef, HostListener} from '@angular/core';
+import {Component, ElementRef, HostListener, ViewChild} from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-navbar',
@@ -9,14 +10,15 @@ import { Router } from '@angular/router';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  menuOpen = false;
+  mobileMenuOpen = false;
+  @ViewChild('menuMo', { static: true }) menuMo!: ElementRef;
 
   constructor(
     public auth: AuthService,
     private router: Router,
     private eRef: ElementRef
-  ) { }
-
-  menuOpen = false;
+  ) {}
 
   goToProfile() {
     this.router.navigate(['/account']);
@@ -26,15 +28,44 @@ export class NavbarComponent {
     this.menuOpen = !this.menuOpen;
   }
 
-  closeMenu() {
-    setTimeout(() => this.menuOpen = false, 150);
+  toggleMobileMenu() {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+    const body = document.body;
+
+    if (this.mobileMenuOpen) {
+      gsap.set(this.menuMo.nativeElement, { display: 'flex' });
+      gsap.fromTo(
+        this.menuMo.nativeElement,
+        { y: '-100%', opacity: 0 },
+        { y: '0%', opacity: 1, duration: 0.5 }
+      );
+      body.classList.add('no-scroll');
+    } else {
+      gsap.to(this.menuMo.nativeElement, {
+        y: '-100%',
+        opacity: 0,
+        duration: 0.4,
+        onComplete: () => {
+          gsap.set(this.menuMo.nativeElement, { display: 'none' });
+          body.classList.remove('no-scroll');
+        }
+      });
+    }
   }
 
   @HostListener('document:click', ['$event'])
   handleClickOutside(event: MouseEvent) {
-    if (!this.eRef.nativeElement.contains(event.target)) {
+    if (
+      this.mobileMenuOpen &&
+      !this.eRef.nativeElement.contains(event.target as Node)
+    ) {
+      this.toggleMobileMenu();
+    }
+    if (
+      this.menuOpen &&
+      !this.eRef.nativeElement.contains(event.target as Node)
+    ) {
       this.menuOpen = false;
     }
   }
-
 }
