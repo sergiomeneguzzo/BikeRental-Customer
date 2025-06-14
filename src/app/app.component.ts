9ca045f19adbf0e10b57a95b928d23d688097e51
sergiomeneguzzo <sergio03.mene@gmail.com>
2025-06-14 14:54:35 +0200

    Routing Landing Lenis Scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NotificationService } from './services/notification.service';
 import { AuthService } from './services/auth.service';
+import Lenis from 'lenis';
+import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
+import {filter} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,21 +12,34 @@ import { AuthService } from './services/auth.service';
   standalone: false,
 })
 export class AppComponent {
-  isLoading = false;
-  currentUser$;
+  private lenis = new Lenis({
+    duration: 1.2,
+    easing: t => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  });
 
   constructor(
-    public authSrv: AuthService,
-    private notificationService: NotificationService
-  ) {
-    this.currentUser$ = this.authSrv.currentUser$;
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
-    this.authSrv.loading$.subscribe((loading) => {
-      this.isLoading = loading;
-    });
-  }
-
-  logout(): void {
-    this.authSrv.logout();
+  ngAfterViewInit() {
+    const raf = (time: number) => {
+      this.lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+    requestAnimationFrame(raf);
+    this.router.events
+      .pipe(filter(e => e instanceof NavigationEnd))
+      .subscribe(() => {
+        const fragment = this.route.snapshot.fragment;
+        if (fragment) {
+          setTimeout(() => {
+            const el = document.getElementById(fragment);
+            if (el) {
+              this.lenis.scrollTo(el, { offset: 0 });
+            }
+          }, 50);
+        }
+      });
   }
 }
